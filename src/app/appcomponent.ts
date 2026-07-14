299b67b101f38919ed127392c7f523346424d67d
Anderson freires <andersonfreires1979@gmail.com>
2026-07-14 15:44:05 -0300

    chore: Inclusão do Dockerfile para containerização da aplicação Angular

---
+++ src/app/appcomponent.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class AppComponent {
+
+  title = signal('hello');
+
+  mensagem: string = 'Eu sou desenvolvedor senior!';
+  nome: string = 'Anderson Freires';
+  idade: number = 47;
+  imagem: string = 'carnival.jpg';
+  off: boolean = false;
+  cor: string = 'green';
+}
