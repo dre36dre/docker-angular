b7a18dbeafac5dac6fbc58ecc3fab680da1c9939
Anderson freires <andersonfreires1979@gmail.com>
2026-07-16 13:05:54 -0300

    feat(angular): add two-way data binding with ngModel

--- src/app/appcomponent.ts
+++ src/app/appcomponent.ts
@@ -1,35 +1,35 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule,FormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class AppComponent {
   contador = 0;
-
+  nome="Anderson";
+  cidade="São Paulo";
   somar() { this.contador++; }
   diminuir() { this.contador--; }
   zerar() { this.contador = 0; }
   dobrar() { this.contador *= 2; }
 
+somarCinco(){
+ this.contador+=5;
+}
+
 
 mensagem: string = 'Eu sou desenvolvedor senior!';
-  nome: string = 'Anderson Freires';
+
   idade: number = 47;
   imagem: string = 'images.png';
   off: boolean = false;
   cor: string = 'green';
 
 
-
-
-
-clicou(){
-  alert("Oi");
-}
 }
