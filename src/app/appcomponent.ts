e5b5606ed0e092ca272a80e00c2bfee50458aed9
Anderson freires <andersonfreires1979@gmail.com>
2026-07-23 16:36:45 -0300

    feat: implement conditional rendering with @if and @else

--- src/app/appcomponent.ts
+++ src/app/appcomponent.ts
@@ -1,48 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule,FormsModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule, FormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class AppComponent {
- nome: string = "";
 
-
-
+  mostrar = true;
+  nome = "";
+  idade = 47;
+  cidade = "São Paulo";
   contador = 0;
+  mensagem = "Eu sou desenvolvedor senior!";
+  imagem = "images.png";
+  off = false;
+  cor = "green";
 
-  cidade="São Paulo";
   somar() { this.contador++; }
   diminuir() { this.contador--; }
   zerar() { this.contador = 0; }
   dobrar() { this.contador *= 2; }
+  somarCinco() { this.contador += 5; }
 
-somarCinco(){
- this.contador+=5;
-}
-
-
-atualizaMensagem(){
-  this.mensagem=
-  'Hello ${this.nome}
-  Age: ${this.idade}
-  City: ${this.cidade}';
-}
-
-
-mensagem: string = 'Eu sou desenvolvedor senior!';
-
-  idade: number = 47;
-  imagem: string = 'images.png';
-  off: boolean = false;
-  cor: string = 'green';
-
-
+  atualizaMensagem() {
+    this.mensagem = `Hello ${this.nome}
+Age: ${this.idade}
+City: ${this.cidade}`;
+  }
 }
